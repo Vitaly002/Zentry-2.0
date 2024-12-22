67d1269e2f25f0dc9a7ae532b42a4b8a1e3b510b
tvto-ipc <152357587+Vitaly002@users.noreply.github.com>
2024-12-22 00:12:10 -0500

    add gaming project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {About, Contact, Features, Footer, Hero, Navbar, Story} from './components';
+
+const App = () => {
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Hero from './Hero';
+import About from './About';
+import Navbar from './Navbar';
+import Features from './Features';
+import Story from './Story';
+import Contact from './Contact';
+import Footer from './Footer';
+
+export {
+    Hero,
+    About,
+    Navbar,
+    Features,
+    Story,
+    Contact,
+    Footer
+}
